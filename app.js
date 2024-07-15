b47c2a53239e99517eae231d4fe0bd1258732a2f
haijames32 <feckhai2003@gmail.com>
2024-07-15 23:26:16 +0700

    update fearture delete product to trash

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var dotenv = require('dotenv')
+var methodOverride = require('method-override')
 
 var mainRouter = require('./routes/main');
 var usersRouter = require('./routes/users');
@@ -23,6 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
 
 app.use('/', mainRouter);
 app.use('/users', usersRouter);
