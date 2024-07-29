5da6fc88c0b8dca513e291d1d88e7a329d722b12
haijames32 <feckhai2003@gmail.com>
2024-07-29 21:07:00 +0700

    update controller api

---
+++ controllers/api/authController.js
@@ -0,0 +1,87 @@
+const myModel = require('../../models/MyModel')
+const { hashPassword, checkHashedPassword } = require('../../services/userService')
+
+const login = async (req, res) => {
+   try {
+      const user = await myModel.userModel.findOne({ username: req.body.username }).populate('addressDefault')
+      if (user) {
+         const check = checkHashedPassword(req.body.passwd, user.passwd)
+         if (check)
+            res.status(200).json({ message: 'Đăng nhập thành công', data: user })
+         else
+            res.status(400).json({ message: 'Không đúng mật khẩu' })
+      } else {
+         res.status(400).json({ message: 'Tài khoản không tồn tại' })
+      }
+   } catch (error) {
+      console.log('Error Login: ', error);
+      res.status(400).json({ message: 'Đã xảy ra lỗi' })
+   }
+}
+
+const register = async (req, res) => {
+   try {
+      const {
+         name,
+         username,
+         email,
+         phoneNumber,
+         passwd,
+         dateOfBirth,
+         gender,
+      } = req.body
+      const pw = hashPassword(passwd)
+      const user = await myModel.userModel.findOne({ username: username })
+      if (user) {
+         res.status(400).json({ message: 'Tài khoản đã tồn tại' })
+      } else {
+         const body = new myModel.userModel({
+            name,
+            username,
+            email,
+            phoneNumber,
+            passwd: pw,
+            dateOfBirth,
+            gender: gender ?? 'None',
+         })
+         const newUser = await body.save()
+         res.status(200).json({ message: 'Đăng ký thành công', data: newUser })
+      }
+   } catch (error) {
+      console.log('Error Register: ', error);
+      res.status(400).json({ message: 'Đã xảy ra lỗi' })
+   }
+}
+
+const changePassword = async (req, res) => {
+   try {
+      const id = req.params.id
+      const {
+         oldPasswd,
+         newPasswd,
+         confirmNewPasswd
+      } = req.body
+      const user = await myModel.userModel.findById({ _id: id })
+      const check = checkHashedPassword(oldPasswd, user.passwd)
+      if (check) {
+         if (newPasswd !== confirmNewPasswd) {
+            res.status(400).json({ message: 'Xác nhận mật khẩu phải trùng mật khẩu mới' })
+         } else {
+            const hashPw = hashPassword(newPasswd)
+            await myModel.userModel.findByIdAndUpdate({ _id: id }, { passwd: hashPw })
+            res.status(200).json({ message: 'Đổi mật khẩu thành công', data: hashPw })
+         }
+      } else {
+         res.status(400).json({ message: 'Sai mật khẩu cũ' })
+      }
+   } catch (error) {
+      console.log('Error :', error)
+      res.status(400).json({ message: 'Đã xảy ra lỗi' })
+   }
+}
+
+module.exports = {
+   login,
+   register,
+   changePassword
+}
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const userCtrl = require('../api/userController')
 const prodCtrl = require('../api/productController')
 const cartCtrl = require('../api/cartController')
 const orderCtrl = require('../api/orderController')
+const authCtrl = require('../api/authController')
 
 module.exports = {
    userCtrl,
    prodCtrl,
    cartCtrl,
    orderCtrl,
+   authCtrl
 }
